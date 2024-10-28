f4f742ddc997d84558a0abd9524b431b357e8bb6
Krishna-010 <93834587+Krishna-010@users.noreply.github.com>
2024-10-28 10:45:59 -0400

    Update range_detection.py

--- obj_avoid/range_detection.py
+++ obj_avoid/range_detection.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 
-import rospy
+import rcply
+from rcply.node import Node
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
 class RangeDetection:
     def __init__(self):
-        rospy.init_node('range_detection', anonymous=True)
+        super().__init__('range_detection')
         qos_profile = QoSProfile(reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,depth=10)
         # Publishers and Subscribers
-        self.obstacle_pub = rospy.Publisher('/obstacle_detected', Bool, queue_size=10)
-        self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback,qos_profile)
+        self.obstacle_pub = self.create_publisher(Bool,'/obstacle_detected',10)
+        self.scan_sub = self.create_subscription(LaserScan,'/scan',self.scan_callback,qos_profile)
 
     def scan_callback(self, msg):
         # Check if there are obstacles close to the robot
         obstacle_detected = any(distance < 0.5 for distance in msg.ranges)
         self.obstacle_pub.publish(obstacle_detected)
 
-if __name__ == "__main__":
+def main(args=None):
+    rclpy.init(args=args)
+    node = RangeDetection()
     try:
-        node = RangeDetection()
-        rospy.spin()
-    except rospy.ROSInterruptException:
+        rclpy.spin(node)  # Spin the node
+    except rclpy.exceptions.ROSInterruptException:
         pass
+    finally:
+        node.destroy_node()  # Clean up the node
+        rclpy.shutdown()     # Shutdown the rclpy library
+        
+if __name__ == "__main__":
+    main()
